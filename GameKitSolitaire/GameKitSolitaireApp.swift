e4b1ceac08d0c6d7fc8fbfa800a70661322d0958
duggansh <duggansh@mail.gvsu.edu>
2025-03-25 12:42:15 -0400

    Initial Commit

---
+++ GameKitSolitaire/GameKitSolitaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameKitSolitaireApp.swift
+//  GameKitSolitaire
+//
+//  Created by Shane M. Duggan on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct GameKitSolitaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GameKitSolitaireTests/GameKitSolitaireTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameKitSolitaireTests.swift
+//  GameKitSolitaireTests
+//
+//  Created by Shane M. Duggan on 3/25/25.
+//
+
+import Testing
+@testable import GameKitSolitaire
+
+struct GameKitSolitaireTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
